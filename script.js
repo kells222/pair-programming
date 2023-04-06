578dcfa44c38a096d9eba40038f52f406f1bcbdd
kells222 <126832421+kells222@users.noreply.github.com>
2023-04-06 16:35:33 -0700

    Create script.js
    what we had so far

---
+++ script.js
@@ -0,0 +1,11 @@
+// Create a program that takes two numbers as inputs and outputs the sum of those numbers.
+function add(num1,num2){
+    console.log(num1+num2)
+}
+
+add(2,3)
+add(6,6)
+add(342,651)
+
+// Write a function that takes a number as input and checks if it is even or odd. If the number is even, return true; if it is odd, return false.
+
